23c3cfcd3c228d31173c6d642b0996c49c0af530
Alexey <alexey-lopatenko@yandex.ru>
2022-06-03 15:56:14 +0300

    Add block travel

--- app/js/main.js
+++ app/js/main.js
@@ -28,4 +28,11 @@ $(function(){
         asNavFor: '.surf-slider',
         focusOnSelect: true
     });
+
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt=""></img>',
+    });
 });
